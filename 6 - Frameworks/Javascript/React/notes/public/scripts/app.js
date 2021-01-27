bd19d62c9eb5a3a81669ad2fe8ce23c2b96479f5
Taimur Aamer <taimur.aamer@gmail.com>
2021-01-27 15:12:00 +0500

    forms

--- 6 - Frameworks/Javascript/React/notes/public/scripts/app.js
+++ 6 - Frameworks/Javascript/React/notes/public/scripts/app.js
@@ -1,18 +1,42 @@
 'use strict';
 
-var user = {
-    name: 'Zakir',
-    age: 21,
-    address: 'Dir, Lower, Maidan',
-    subjects: ['C++', 'Java', 'PHP', 'Javascript', 'React']
-};
+var text = [];
 
-var handle = function handle() {
-    console.log('clicked...');
+var formHandler = function formHandler(e) {
+    e.preventDefault();
+    text.push(e.target.elements.username.value);
+    reRender();
 };
-
-var template = React.createElement('div', null);
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+var reRender = function reRender() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'form',
+            { onSubmit: formHandler },
+            React.createElement('input', { type: 'text', name: 'username' }),
+            React.createElement(
+                'button',
+                null,
+                'Click'
+            ),
+            React.createElement(
+                'ol',
+                null,
+                text.map(function (t) {
+                    return React.createElement(
+                        'li',
+                        null,
+                        t
+                    );
+                })
+            )
+        )
+    );
+    ReactDOM.render(template, appRoot);
+};
+
+reRender();
--- 6 - Frameworks/Javascript/React/notes/src/app.js
+++ 6 - Frameworks/Javascript/React/notes/src/app.js
@@ -1,5 +1,24 @@
-const template = <p>This is it</p>
+let text = [];
+
+const formHandler = (e) => {
+    e.preventDefault();
+    text.push(e.target.elements.username.value);
+    reRender();
+}
 
 const appRoot = document.getElementById('app')
 
-ReactDOM.render(template, appRoot)
+const reRender = () => {
+    const template = <div>
+                        <form onSubmit={formHandler}>
+                            <input type='text' name='username' />
+                            <button>Click</button>
+                            <ol>{text.map((t) => {
+                                return <li>{t}</li>
+                            })}</ol>
+                        </form>
+                    </div>
+    ReactDOM.render(template, appRoot)
+}
+
+reRender();
